4f147756fbe719e55ae7ebe341e3dc29611997b1
Dremnor <konradszostopal@gmail.com>
2023-12-02 15:24:39 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Message from "./Message";
+import FoodTable from "./FoodTable";
+
+function App(){
+  return <div>
+            <div>
+            <Message/>
+            </div>
+            <div>
+            <FoodTable/>
+            </div>
+          </div>;
+}
+
+export default App;
